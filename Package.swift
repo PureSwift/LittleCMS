32871723c3bd58d1dc3532125bd2c21e5acec5e1
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-06-07 02:07:09 -0500

    Revert "Updated for Swift 4"
    This reverts commit 35cbbb4475ec83ccf9606024d6b196e8540197bf.

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "LittleCMS",
-            dependencies: ["CLCMS"]),
+            dependencies: []),
         .testTarget(
             name: "LittleCMSTests",
             dependencies: ["LittleCMS"]),
