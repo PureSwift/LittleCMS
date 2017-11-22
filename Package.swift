4143dfd3916be5b1ccd3cc46c27d939e9b63c0dd
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-11-22 03:01:25 -0500

    Fixed for Swift 3.0.2 compatibility

--- Package.swift
+++ Package.swift
@@ -1,27 +1,14 @@
-// swift-tools-version:4.0
+// swift-tools-version:3.0.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "LittleCMS",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "LittleCMS",
-            targets: ["LittleCMS"]),
+    targets: [
+        Target(name: "LittleCMS")
     ],
     dependencies: [
-        .package(url: "https://github.com/PureSwift/CLCMS.git", .branch("master"))
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "LittleCMS",
-            dependencies: []),
-        .testTarget(
-            name: "LittleCMSTests",
-            dependencies: ["LittleCMS"]),
+        .Package(url: "https://github.com/PureSwift/CLCMS.git", majorVersion: 1)
     ]
 )
